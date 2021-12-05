e92498e8c695b89eeeec516ebf499d1c53a5ecb7
Thomas Burette <burettethomas@gmail.com>
2021-12-05 18:29:47 +0100

    simplify template

--- template/sol.js
+++ template/sol.js
@@ -1,11 +1,6 @@
 "use strict";
 const fs = require('fs')
-//for terminal (node) and VS code (quokka) compatibility
-// in VS Code (and Quokka), cwd is not the directory of this script but the root project
-process.chdir(__dirname);
-// fs.readFile('input', 'utf8', (err, data) => {    
-//     console.log(err, data);
-// });
+
 let data = fs.readFileSync('input', 'utf8');
 data = data.trim().split("\n");
 // data = data.map(Number)
