b36911f76b03bdc0b1313203a8c392596e87cba2
Thomas Burette <burettethomas@gmail.com>
2022-02-09 23:21:43 +0100

    day 10

---
+++ day10/sol.js
@@ -0,0 +1,10 @@
+"use strict";
+const fs = require('fs')
+process.chdir(__dirname); // work when launched from any current directory
+let data = fs.readFileSync('input', 'utf8');
+data = data.trim().split("\n");
+// data = data.map(Number)
+// data = data.map((line)=>line.match(/(-?\d)+/g)); // lines have multiple numbers. Get : [num1, num2]
+// data = data.map((line)=>line.match(/(\d+)(u|d)(\d+)/).slice(1)) // extract groups from regex. Get : [group1, group2]
+data
+console.log(data);
